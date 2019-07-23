184a04245eda3fe13cfb483b50b58a854b4bee3b
Brodie Brock <brodiebrock@gmail.com>
2019-07-23 11:41:17 -0400

    added schema

---
+++ SQL/schema.sql
@@ -0,0 +1,47 @@
+create table employee(
+    id serial primary key,
+    firstname VARCHAR(100),
+    lastname VARCHAR(100),
+    phone VARCHAR(100),
+    email VARCHAR(100),
+    experience VARCHAR(100),
+    datestarted VARCHAR(100),
+    adminstatus VARCHAR(100),
+    course_id INTEGER REFERENCES yourclub(id)
+);
+
+create table sprayChart(
+    id serial primary key,
+    dateApplied varchar(100),
+    employee_id integer references employee(id),
+    holesTreated varchar(1000),
+    lengthOfCutTreated varchar(1000),
+    chemicalsBeingUsed text,
+    rateApplied varchar(100),
+    gallonsPerTank varchar(100),
+    sprayRig varchar(1000),
+    pestOrDiseaseControlled varchar(1000)
+);
+
+create table yourclub(
+    id serial primary key,
+    clubname VARCHAR(200),
+    admin VARCHAR(100),
+    employees VARCHAR(200),
+    citystate VARCHAR(200)
+);
+
+create table jobboard(
+    id serial primary key,
+    date VARCHAR(100),
+    jobtype INTEGER REFERENCES jobtype(id),
+    employee INTEGER REFERENCES employee(id),
+    comments text
+);
+
+create table jobtype(
+    id serial primary key,
+    jobtype VARCHAR(200),
+    instructions VARCHAR(200),
+);
+
