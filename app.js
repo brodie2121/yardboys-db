79c7fa88836f8677193fe024bf4bc3278eadc0ca
Brodie Brock <brodiebrock@gmail.com>
2019-07-23 14:38:26 -0400

    updated app.js file

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+    path = require('path');
+    cookieParser = require('cookie-parser');
+    logger = require('morgan');
+    cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+    usersRouter = require('./routes/users');
+    JobBoardRouter = require('./routes/JobBoard');
+    employeeRouter = require('./routes/employee');
+    sprayChartRouter = require('./routes/sprayChart');
+    JobTypeRouter = require('./routes/jobType');
+    YourCourseRouter = require('./routes/yourCourse');
 
-var app = express();
+const corsOptions = {
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204,
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+};
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors(corsOptions));
 
+app.use('/jobboard', JobBoardRouter);
+app.use('/employee', employeeRouter);
+app.use('/spraychart', sprayChartRouter);
+app.use('/jobtype', JobTypeRouter);
+app.use('/yourcourse', YourCourseRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
